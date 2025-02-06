e3d519f4a63465120fefde11d29910daf3df4bee
Andreas Glifberg Byllemos <anb013@edu.zealand.dk>
2025-02-06 10:11:53 +0100

    hej

--- Models/Accessories.cs
+++ Models/Accessories.cs
@@ -2,30 +2,21 @@
 
 namespace PrinterShop.Models
 {
-    public class Printer:IProduct
+    public class Accessories:IProduct
     {
 
 
-        public Printer(string name, string description, double price)
+        public Accessories(string name, string description, double price)
         {
             Name = name;
             Description = description;
             Price = price;
+
         }
 
-
-
-
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public double Price { get; set; }
-
-
-
-
-
 
 
     }
---
+++ Pages/Opret profil/OpretProfil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrinterShop.Pages.Opret_profil
+{
+    public class OpretProfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
